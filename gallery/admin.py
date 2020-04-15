58c5344b324114d8abfd217edc4c541020fa4404
Doug McClellan <DJMcClellan1966@gmail.com>
2020-04-14 22:43:45 -0400

    got images to align using css across page instead of down

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 from .models import Image
 
 admin.site.register(Image)
+
+class ImageAlbumAdmin(admin.ModelAdmin):
+    fieldsets = (
+    (None, {
+        'fields': ('title', 'images')
+    })
+    )
